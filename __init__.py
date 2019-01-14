121f0e626f26c69f51eca60dd08bee4f90bbe82d
rafaelrejtman <rafaelrejtman@gmail.com>
2019-01-13 19:14:56 -0800

    database Incoporation

--- __init__.py
+++ __init__.py
@@ -3,7 +3,11 @@ import pickle
 from flask import Flask, render_template
 from flask import request, redirect, g
 import os
+import psycopg2
+from psycopg2 import Error
 # import sqlite3
+
+DATABASE_URL = os.environ['DATABASE_URL']
 
 app = Flask(__name__)
 
@@ -12,14 +16,20 @@ with open("model.pkl", 'rb') as file:
 
 port = int(os.environ.get("PORT", 5000))
 
-# @app.before_request
-# def before_request():
-# 	g.db = sqlite3.connect("emails.db")
+@app.before_request
+def before_request():
+    try:
+        connection = psycopg2.connect(DATABASE_URL, sslmode='require')
+        cursor = connection.cursor()
+    except (Exception, psycopg2.Error) as error ::
+         print ("Error while connecting to PostgreSQL", error)
 
-# @app.teardown_request
-# def teardown_request(exception):
-# 	if hasattr(g, 'db'):
-# 		g.db.close()	
+@app.teardown_request
+def teardown_request(exception):
+    if(connection):
+        cursor.close()
+        connection.close()
+        print("PostgreSQL connection is closed")    
 
 @app.route('/result', methods=['GET'])
 def result():
@@ -40,7 +50,22 @@ def index():
 
 @app.route('/insert')
 def insert():
-		return render_template('insert.html')    
+		return render_template('insert.html')  
+
+
+@app.route('/database')
+def database():
+        
+    create_table_query = '''CREATE TABLE mobile
+          (ID INT PRIMARY KEY     NOT NULL,
+          MODEL           TEXT    NOT NULL,
+          PRICE         REAL); '''
+    
+    cursor.execute(create_table_query)
+    connection.commit()
+    print("Table created successfully in PostgreSQL ")        
+    
+    return redirect('/')    
 
 # @app.route('/signup', methods = ['POST'])
 # def signup():
